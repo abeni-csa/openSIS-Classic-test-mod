0c5a7b4620ab6b360c2b856f6b9cea7623e83379
Abenezer Haymanot <143000984+abeni-csa@users.noreply.github.com>
2025-03-05 06:25:02 -0800

    Update Ajax_url_encode.php

--- Ajax_url_encode.php
+++ Ajax_url_encode.php
@@ -29,7 +29,7 @@
 
 require_once 'functions/UrlFnc.php';
 
-$encoded_url = encode_url($_REQUEST['link_url']);
+$encoded_url = encode_url($_REQUEST["link_url"]);
 
 echo htmlentities($encoded_url);
 
